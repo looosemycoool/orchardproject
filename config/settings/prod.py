9b023fee405ddfea86a5b1375cfb93a7e821151e
Jungyoon Han <jyoon0c11@gmail.com>
2023-06-25 13:44:44 +0900

    prod.py 호스트 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,8 +2,8 @@ import environ
 
 from .base import *
 
-ALLOWED_HOSTS = ['43.200.128.160']
-STATIC_ROOT = BASE_DIR / 'static/'
+ALLOWED_HOSTS = ['43.200.128.160', 'gwasuwon.kr']
+STATIC_ROOT = BASE_DIR / 'orchard/static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
