c0d8802f2d8e52136ffeea38c73328b6c6a5b395
Jungyoon Han <jyoon0c11@gmail.com>
2023-06-26 15:15:11 +0900

    docs/admin page list_display & search_field 수정

--- reserve/admin.py
+++ reserve/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from .models import Reserve, Time_Table, Teacher, Notice
 
 class ReserveAdmin(admin.ModelAdmin):
-    search_fields = ['']
+    list_display = ('date', 'time', 'teacher_id', 'student_name', 'comment_subject', "comment_content")
+    search_fields = ["teacher_id__name", "student_name__username", "date"]
 
-admin.site.register(Reserve)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'duty_day', 'duty_time')
+    search_fields = ["name"]
+
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ('notice_header', 'notice_body')
+
+
+admin.site.register(Reserve, ReserveAdmin)
 admin.site.register(Time_Table)
-admin.site.register(Teacher)
-admin.site.register(Notice)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Notice, NoticeAdmin)
--- teacher/admin.py
+++ teacher/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Consulting
 
 class ConsultingAdmin(admin.ModelAdmin):
-    list_display = ('date', 'teacher_id', 'student_name')
-    search_fields = ["student_name"]
+    list_display = ('date', 'teacher_id', 'student_name', 'consulting_type', 'consulting_subject', 'consulting_content')
+    search_fields = ["teacher_id_name", "student_name"]
 
 admin.site.register(Consulting, ConsultingAdmin)
