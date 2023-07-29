26c44f8ea8787428576617afe6391858f7b9a0ab
Jungyoon Han <jyoon0c11@gmail.com>
2023-07-29 15:37:45 +0900

    content 길이 조정

--- reserve/models.py
+++ reserve/models.py
@@ -28,7 +28,7 @@ class Reserve(models.Model):
     comment = models.CharField(max_length=1000, null=True, blank=True)
     type = models.CharField(max_length=50, null=True, blank=True)
     subject = models.CharField(max_length=100, null=True, blank=True)
-    content = models.TextField(max_length=300, null=True, blank=True)
+    content = models.TextField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return str(self.date) if self.date else ''
