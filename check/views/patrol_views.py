c82a4fc748e54a9405a2ce6c220ea0ba460d7e6f
Jungyoon Han <jyoon0c11@gmail.com>
2023-12-30 06:55:31 +0900

    왜 서버 에러 500이 날까

--- check/views/patrol_views.py
+++ check/views/patrol_views.py
@@ -46,6 +46,7 @@ def create_patrol(request, student_register_id, selected_date):
 def patrol_p_class(request):
     current_date = datetime.now().date()
     patrol_p = PatrolCheck.objects.filter(user__class_name='P', date=current_date, user__is_dropped=False).order_by('user__class_num')
+
     line1 = ['50', '51', '52', '53', '54', '55', '56', '57', '58', '59']
     line2 = ['41', '42', '43', '44', '45', '46', '47', '48', '49']
     line3 = ['31', '32', '33', '34', '35', '36', '37', '38', '39', '40']
@@ -53,7 +54,7 @@ def patrol_p_class(request):
     line5 = ['12', '13', '14', '15', '16', '17', '18', '19', '20']
     line6 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
 
-    context = {'patrol_p': patrol_p, 'line1': line1, 'line2': line2, 'line3': line3, 'line4':line4, 'line5':line5, 'line6':line6}
+    context = {'patrol_p': patrol_p, 'line1': line1, 'line2': line2, 'line3': line3, 'line4': line4, 'line5':line5, 'line6':line6}
     return render(request, 'check/patrol_p_class.html', context)
 
 def patrol_check_p(request, patrol_id):
