31968a38e31684d62a0c47f742df9ac5a36d5586
Jungyoon Han <jyoon0c11@gmail.com>
2024-01-07 04:30:06 +0900

    static 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ import environ
 from .base import *
 
 ALLOWED_HOSTS = ['43.200.128.160', 'gwasuwon.kr']
-STATIC_ROOT = BASE_DIR / 'orchard/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
