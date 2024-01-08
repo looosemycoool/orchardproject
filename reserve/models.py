89893b6115743291ab170c1914a06a4b46877738
Jungyoon Han <jyoon0c11@gmail.com>
2024-01-09 01:54:48 +0900

    title 추가

--- reserve/models.py
+++ reserve/models.py
@@ -29,7 +29,7 @@ class Reserve(models.Model):
     subject = models.CharField(max_length=100, null=True, blank=True)
     comment = models.CharField(max_length=1000, null=True, blank=True)
     type = models.CharField(max_length=50, null=True, blank=True)
-    # title = models.CharField(max_length=100, null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
     content = models.TextField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
