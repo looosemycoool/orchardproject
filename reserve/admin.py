884ec538ed6ea681828189a7ee0047566088c35a
Jungyoon Han <jyoon0c11@gmail.com>
2024-01-22 14:10:03 +0900

    admin 질문 예약 title 보이도록

--- reserve/admin.py
+++ reserve/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Reserve, Time_Table, Teacher, Notice
 
 class ReserveAdmin(admin.ModelAdmin):
-    list_display = ('date', 'time', 'teacher_id', 'student_name', 'comment', 'subject')
+    list_display = ('date', 'time', 'teacher_id', 'student_name', 'title', 'subject')
     search_fields = ["teacher_id__name", "student_name__first_name", "date"]
 
 class TeacherAdmin(admin.ModelAdmin):
